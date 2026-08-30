c1a1dda525c1c670f356af1a5a68c55e35ce8a3a
Daniel Solarte <66956234+dsolartec@users.noreply.github.com>
2026-08-29 22:11:43 -0500

    feat: implement clean architecture

--- src/adapters/http/app.py
+++ src/adapters/http/app.py
@@ -1,6 +1,7 @@
 from flask import Flask
 
-from app.blueprints.ping_views import ping_views
+from src.adapters.http.views.ping_views import ping_views
+
 
 def create_app():
     app = Flask(__name__)
