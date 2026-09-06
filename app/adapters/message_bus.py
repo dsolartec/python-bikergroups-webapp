774136e3f4c448f73812205742866e81b5ec6a11
Daniel Solarte <66956234+dsolartec@users.noreply.github.com>
2026-09-06 12:12:12 -0500

    fix: remove unused import

--- app/adapters/message_bus.py
+++ app/adapters/message_bus.py
@@ -1,4 +1,4 @@
-from typing import Any, Callable, Type
+from typing import Callable, Type
 
 from app.domain.commands.base_command import BaseCommand
 from app.domain.ports.abstract_container import AbstractContainer
