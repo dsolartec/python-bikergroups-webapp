58dddb8d6920a16d80221dbb6fff562319d46200
Daniel Solarte <66956234+dsolartec@users.noreply.github.com>
2026-09-06 00:02:09 -0500

    feat: add sign up page

--- app/adapters/inputs/web_views.py
+++ app/adapters/inputs/web_views.py
@@ -8,7 +8,9 @@ from app.bootstrap import bootstrap
 from app.domain.commands.get_all_roadtrips_paginated_command import GetAllRoadtripsPaginatedCommand
 from app.domain.commands.get_user_by_id_command import GetUserByIDCommand
 from app.domain.commands.signin_command import SignInCommand
+from app.domain.commands.signup_command import SignUpCommand
 from app.domain.exceptions.not_found_exception import NotFoundException
+from app.domain.exceptions.phone_already_exists_exception import PhoneAlreadyExistsException
 from app.domain.exceptions.wrong_credentials_exception import WrongCredentialsException
 from app.domain.models.roadtrip_model import RoadTripModel
 from app.domain.models.user_model import UserModel
@@ -73,6 +75,88 @@ def signin_page():
             flash("El número de teléfono o la contraseña no son válidos", "signin.error")
 
     return render_template("auth/signin.html")
+
+
+@web_views.route("/register", methods=[HTTPMethod.GET, HTTPMethod.POST])
+@require_unlogged()
+def signup_page():
+    confirm_password: str = ""
+    display_name: str = ""
+    password: str = ""
+    phone: str = ""
+
+    if request.method == HTTPMethod.POST:
+        flashed_message: bool = False
+
+        display_name = request.form["display_name"]
+        if len(display_name) < 4 or len(display_name) > 100:
+            flash("El nombre visible debe tener entre 4 y 100 caracteres", "signup.display_name.error")
+            flashed_message = True
+
+        phone = request.form["phone"]
+        if len(phone) != 10 or not phone.startswith("3"):
+            flash("El número de teléfono no es válido", "signup.phone.error")
+            flashed_message = True
+
+        password = request.form["password"]
+        if len(password) < 8:
+            flash("La contraseña debe tener mínimo 8 caracteres", "signup.password.error")
+            flashed_message = True
+        elif len(password) > 80:
+            flash("La contraseña solo puede tener máximo 80 caracteres", "signup.password.error")
+            flashed_message = True
+        else:
+            numbers_count: int = 0
+            lowers_count: int = 0
+            uppers_count: int = 0
+
+            for character in password:
+                if character.isnumeric():
+                    numbers_count += 1
+                elif character.islower():
+                    lowers_count += 1
+                elif character.isupper():
+                    uppers_count += 1
+
+            if uppers_count == 0:
+                flash("La contraseña debe tener mínimo una letra mayúscula", "signup.password.error")
+                flashed_message = True
+            elif lowers_count == 0:
+                flash("La contraseña debe tener mínimo una letra minúscula", "signup.password.error")
+                flashed_message = True
+            elif numbers_count == 0:
+                flash("La contraseña debe tener mínimo un número", "signup.password.error")
+                flashed_message = True
+
+        confirm_password = request.form["confirm_password"]
+        if password != confirm_password:
+            flash("Las contraseñas no coinciden", "signup.confirm_password.error")
+            flashed_message = True
+
+        if not flashed_message:
+            try:
+                user: UserModel = message_bus.handle(SignUpCommand(
+                    display_name=display_name,
+                    password=password,
+                    phone=phone
+                ))
+
+                session.clear()
+                session["user_id"] = user.id
+
+                return redirect(url_for("web_views.landing_page"))
+            except PhoneAlreadyExistsException:
+                flash("El número de teléfono ya está en uso", "signup.phone.error")
+
+    return render_template(
+        "auth/signup.html",
+        form_data={
+            "confirm_password": confirm_password,
+            "display_name": display_name,
+            "password": password,
+            "phone": phone,
+        },
+    )
 
 
 @web_views.route("/logout", methods=[HTTPMethod.GET])
